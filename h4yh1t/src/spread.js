e42e04337feb41647b86d43f6b84ab15a6fbf816
이유진 <rooproop1111@naver.com>
2023-03-06 21:36:02 +0900

    front-memberlist
    - flexbox

---
+++ h4yh1t/src/spread.js
@@ -0,0 +1,38 @@
+let users = [{}];
+const container = document.getElementById("container");
+
+function GET() {
+  var xhr = new XMLHttpRequest();
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        console.log("연결 성공");
+        users = JSON.parse(xhr.response);
+        users.map((obj) => {
+          console.log("arr : " + obj);
+          let card = document.createElement("div");
+          card.className = "card";
+          card.innerHTML = `
+          <div> \
+            <div> \
+              <div>${obj.username}(${obj.name})</div> \
+              <div>${obj.email}</div> \
+            </div> \
+            <div> \
+              <div>${obj.phone}</div> \
+              <div>${obj.website}</div> \
+            </div> \
+          </div>`;
+          container.appendChild(card);
+        });
+      } else {
+        console.log("연결 실패");
+      }
+    }
+  };
+  xhr.open("GET", "https://jsonplaceholder.typicode.com/users", true);
+  xhr.send();
+}
+
+GET();
